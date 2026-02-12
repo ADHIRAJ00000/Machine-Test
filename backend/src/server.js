68a14f07276e5485c6b9c2f920ce8a314479c009
Adhiraj SIngh <162025361+ADHIRAJ00000@users.noreply.github.com>
2026-02-13 02:43:49 +0530

    Update CORS middleware configuration

--- backend/src/server.js
+++ backend/src/server.js
@@ -11,15 +11,13 @@ connectDB();
 
 const app = express();
 
-// Middleware
-<<<<<<< HEAD
+
 app.use(cors({
     origin: process.env.CORS_ORIGIN || '*',
     credentials: true,
 }));
-=======
-app.use(cors());
->>>>>>> c5cb1bd4b96ab4c7275e81b709ca115f32511f6d
+
+
 app.use(express.json());
 app.use('/uploads', express.static(path.join(__dirname, '../uploads')));
 
@@ -34,7 +32,7 @@ app.get('/', (req, res) => {
     res.send('API is running...');
 });
 
-// Basic Error Handling Middleware
+
 app.use((err, req, res, next) => {
     const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
     res.status(statusCode);
