154506086f51d7ee6b80e0acb3de4a91fca1577d
Adhiraj SIngh <162025361+ADHIRAJ00000@users.noreply.github.com>
2026-02-13 02:54:54 +0530

    Update API proxy URL in Vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,16 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+
 export default defineConfig({
     plugins: [react()],
     server: {
         proxy: {
-<<<<<<< HEAD
+
             '/api': 'http://localhost:5002',
-=======
-            '/api': 'https://machine-test-10.onrender.com',
->>>>>>> c5cb1bd4b96ab4c7275e81b709ca115f32511f6d
+
+
         },
     },
 })
