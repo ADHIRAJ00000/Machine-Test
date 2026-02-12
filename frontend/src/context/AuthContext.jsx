de92e78aea7875a5c800011b5d332d289c9b4587
Adhiraj SIngh <162025361+ADHIRAJ00000@users.noreply.github.com>
2026-02-13 02:59:25 +0530

    Replace axios with custom api for authentication

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -1,9 +1,5 @@
 import { createContext, useState, useEffect, useContext } from 'react';
-<<<<<<< HEAD
 import api from '../api/axiosConfig';
-=======
-import axios from 'axios';
->>>>>>> c5cb1bd4b96ab4c7275e81b709ca115f32511f6d
 
 const AuthContext = createContext();
 
@@ -15,21 +11,13 @@ export const AuthProvider = ({ children }) => {
     );
 
     const login = async (email, password) => {
-<<<<<<< HEAD
         const { data } = await api.post('/api/users/login', { email, password });
-=======
-        const { data } = await axios.post('/api/users/login', { email, password });
->>>>>>> c5cb1bd4b96ab4c7275e81b709ca115f32511f6d
         setUserInfo(data);
         localStorage.setItem('userInfo', JSON.stringify(data));
     };
 
     const register = async (name, email, password) => {
-<<<<<<< HEAD
         const { data } = await api.post('/api/users', { name, email, password });
-=======
-        const { data } = await axios.post('/api/users', { name, email, password });
->>>>>>> c5cb1bd4b96ab4c7275e81b709ca115f32511f6d
         setUserInfo(data);
         localStorage.setItem('userInfo', JSON.stringify(data));
     };
